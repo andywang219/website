ec15fec10b0e9a75d41fa482f57c5c2a4544c5a3
Andy Wang <andywang219@yahoo.com>
2018-11-01 01:52:48 -0400

    updated design

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,11 +4,11 @@ import './styleItUp.css';
 const Contact = () => {
     return (
         <div style = {{display: "inline-block", marginTop: "40px"}}>
-            <h2 id = "headings">Contact Me!</h2>
+            <h2 className = "headings">Contact Me!</h2>
             <div>
                 <a href = "https://www.facebook.com/andy.wang.52035" rel = "noopener noreferrer" target = "_blank"><i className = "fab fa-facebook-square fa-3x" style = {{color: "rgb(59, 89, 152)"}}></i></a>
                 <a href = "https://www.linkedin.com/in/andy-wang-9063a215b/" rel = "noopener noreferrer" target = "_blank"><i className = "fab fa-linkedin fa-3x" style = {{color: "rgb(0, 119, 181)"}}></i></a>
-                <a href = "https://github.com/andywang219" rel = "noopener noreferrer" target = "_blank"><i className = "fab fa-github-square fa-3x " style = {{color: "rgb(0, 0, 0)"}}></i></a>
+                <a href = "https://github.com/andywang219" rel = "noopener noreferrer" target = "_blank"><i className = "fab fa-github-square fa-3x " style = {{color: "rgb(89, 93, 102)"}}></i></a>
                 <address><a href = "mailto:andywang219@gmail.com"><i className = "fas fa-envelope fa-3x" style = {{color: "rgb(175, 10, 50)"}}></i></a></address>
             </div>
         </div>
--- src/Components/Exp.js
+++ src/Components/Exp.js
@@ -6,14 +6,17 @@ class Exp extends Component {
         super(props);
         this.state = {
             lang: ["HTML", "CSS", "JavaScript", "reactJS", "nodeJS", "Java", "Python", "SQL", "Microsoft Word", "Microsoft Excel"],
-            image: ["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-react", "fab fa-node", "fab fa-java", "fab fa-python", "fas fa-database", "fas fa-file-word", "fas fa-file-excel"]
+            image: ["fab fa-html5", "fab fa-css3-alt", "fab fa-js-square", "fab fa-react", "fab fa-node", "fab fa-java", "fab fa-python", "fas fa-database", "fas fa-file-word", "fas fa-file-excel"],
+            color: ["#e34f26", "#0037fb", "#f7df1e", "#00d8ff", "#6cc24a", "#1f4a94", "#00758f", "#0078d7", "#00a1f1", "#7cbb00"]
         }
     }
     render() {
         const skills = this.state.lang.map((data, i) => {
                 return (
                     <div className = "skillSet" key = {i}>
-                        <li className = "skills"><i className = {this.state.image[i]}></i> {data} </li>
+                        <li className = "skills">
+                            <i className = {this.state.image[i]} style = {{color: this.state.color[i]}}>
+                        </i> {data} </li>
                     </div>
                 )
             }
@@ -21,12 +24,16 @@ class Exp extends Component {
         return (
             <div>
                 <h1 className = "headings">Leadership & Experience</h1>
-                    <p className = 'bodyText'><em>Ten-Tan Chinese Restaurant</em> Feb 2015-Present</p>
-                    <p className = "subBody">
-                        I am currently working as a waiter at a privately owned restaurant. My responsiblities include customer services, handling orders, and restocking inventory. Even though this job is not related to Computer Science, it has taught me many skills that are applicable. Examples of such skills are communication, diligence, precision, multi-tasking, and leadership.
+                    <p className = 'bodyText'><em>Ten-Tan Chinese Restaurant</em> <br/> Feb 2015 - October 2018</p>
+                    <p className = "bodyText">
+                        I used to work as a waiter at a privately owned restaurant. This job was like a combination of many jobs. I learned how to adapt and help out other employees when the workload began to build up. Even though this job is not related to the Computer Science field, it has taught me many skills that are applicable. I've learned
+                            <li>how to work in an agile environment,</li> 
+                            <li>how to communicate with other employees, </li> 
+                            <li>how to solve issues when they arise, </li>
+                            <li>and most importantly how anything is possible if you stay committed. </li>
                     </p>
                 <h1 className = "subHeadings">Skill Set</h1>
-                {skills}
+                <div id = "skillBlock"> {skills} </div>
             </div>
         )
     }
